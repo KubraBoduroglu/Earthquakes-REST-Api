47db32d48df931973ee6219b0ab5f2bd2e41c354
Kubra Boduroglu <k.boduroglu@outlook.com>
2023-07-03 17:01:49 +0300

    minor fixes in EarthquakesServiceImpl

--- src/main/java/com/kubraboduroglu/projects/earthquakes/service/EarthquakesServiceImpl.java
+++ src/main/java/com/kubraboduroglu/projects/earthquakes/service/EarthquakesServiceImpl.java
@@ -4,29 +4,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.core.publisher.Mono;
 
+@Service
 public class EarthquakesServiceImpl implements EarthquakesService{
 
 	RestTemplate restTemplate = new RestTemplate();
 	private static final String USGS_URL = "https://earthquake.usgs.gov/fdsnws/event/1/";
+	private static final String countUrl = "https://earthquake.usgs.gov/fdsnws/event/1/count?starttime={starttime}&endtime={endtime}";
 	
 	Map<String, String> paramMap = new HashMap<>();
 	
 	//WebClient client = WebClient.create();
 	WebClient webClient = WebClient.builder()
-		    .baseUrl(USGS_URL)
+		    .baseUrl("http://localhost:8080")
 		    .build();
 	
 	public String getCountObject(String startTime, String endTime) {
 		paramMap.put("starttime", startTime);
 		paramMap.put("endtime", endTime);
-		
-		String countUrl = "https://earthquake.usgs.gov/fdsnws/event/1/count?starttime={starttime}&endtime={endtime}";
-		
+				
 		String count = restTemplate.getForObject(countUrl, String.class, paramMap);
 		return count;
 	}
@@ -35,20 +36,23 @@ public class EarthquakesServiceImpl implements EarthquakesService{
 		
 		paramMap.put("starttime", startTime);
 		paramMap.put("endtime", endTime);
-		
-		String countUrl = "https://earthquake.usgs.gov/fdsnws/event/1/count?starttime={starttime}&endtime={endtime}";
-		
+				
 		ResponseEntity<String> count = restTemplate.getForEntity(countUrl, String.class, paramMap);
 		return count;
 	}
 
 	@Override
 	public Mono<String> getCountWithWebClient(String startTime, String endTime) {
-		Mono<String> result = webClient.get()
-				.uri(USGS_URL, paramMap)
+		
+		paramMap.put("starttime", startTime);
+		paramMap.put("endtime", endTime);
+		
+		Mono<String> count = webClient.get()
+				.uri(countUrl, paramMap)
 				.retrieve()
 				.bodyToMono(String.class);
-		return result;
+				//.block();
+		return count;
 	}
 	
 	
