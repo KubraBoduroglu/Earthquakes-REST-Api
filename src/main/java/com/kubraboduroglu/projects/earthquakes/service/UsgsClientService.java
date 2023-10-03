3df5db92363451d98ffd7878af6d73ab8897c128
nihal <nihal.ozturk1@gmail.com>
2023-10-03 19:13:24 +0300

    Fix return types

--- src/main/java/com/kubraboduroglu/projects/earthquakes/service/UsgsClientService.java
+++ src/main/java/com/kubraboduroglu/projects/earthquakes/service/UsgsClientService.java
@@ -1,20 +1,20 @@
 package com.kubraboduroglu.projects.earthquakes.service;
 
-import org.springframework.http.ResponseEntity;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.kubraboduroglu.projects.earthquakes.dto.UsgsQueryReqDTO;
 import com.kubraboduroglu.projects.earthquakes.dto.UsgsResponseDTO;
-
-import reactor.core.publisher.Mono;
+import org.springframework.http.ResponseEntity;
 
 public interface UsgsClientService {
 
-	String getCountObject(String startTime, String endTime);
-	ResponseEntity<String> getCountEntity(String startTime, String endTime);
-	ResponseEntity<String> getCountWithWebClient(String startTime, String endTime);
-	String getUsgsData(String startTime, String endTime, Integer minMagnitude);
-	//String getUsgsDatav2(String startTime, String endTime, Integer minMagnitude);
-	UsgsResponseDTO[] getUsgsDatav2(UsgsQueryReqDTO usgsQueryReqDto) throws JsonMappingException, JsonProcessingException;
+    String getCountObject(String startTime, String endTime);
+
+    ResponseEntity<String> getCountEntity(String startTime, String endTime);
+
+    ResponseEntity<String> getCountWithWebClient(String startTime, String endTime);
+
+    String getUsgsData(String startTime, String endTime, Integer minMagnitude);
+
+    //String getUsgsDatav2(String startTime, String endTime, Integer minMagnitude);
+    UsgsResponseDTO getUsgsDatav2(UsgsQueryReqDTO usgsQueryReqDto) throws JsonProcessingException;
 }
